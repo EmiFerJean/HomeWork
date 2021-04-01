fcddbf8c5dd10079bd26bcd189875fe0cd0bb480
Emilio Fernandez <emi.fer.jean@gmail.com>
2021-04-01 19:08:58 +0200

    update

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ package HomeWork;
 public class Main {
     public static void main (String[] args) {
         System.out.println("I will be a good Java Developer!");
-        System.out.println("I will be a good Java Developer!");
-        System.out.println("I will be a good Java Developer!");
-        System.out.println("I will be a good Java Developer!");
-        System.out.println("I will be a good Java Developer!");
+        System.out.println("Oh yeah!");
     }
 }
