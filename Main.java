bf698428185f2bc97521938dda5af060b32e1d4a
Emilio Fernandez <emi.fer.jean@gmail.com>
2021-04-01 19:24:49 +0200

    new

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("I will be a good Java Developer!");
         System.out.println("Oh yeah!");
+        System.out.println("Oh yeah!");
     }
 }
