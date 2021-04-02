1bdf97c88d30b76d1dffa54d471c21aed3d92c93
Emilio Fernandez <emi.fer.jean@gmail.com>
2021-04-02 18:33:44 +0200

    New

---
+++ src/com/emilio/CountBackwards.java
@@ -0,0 +1,10 @@
+package com.emilio;
+
+public class CountBackwards {
+    public static void main(String[] args) {
+        int count;
+        for (count = 10; count > 0; count--) {
+            System.out.println(count);
+        }
+    }
+}
---
+++ src/com/emilio/Counter.java
@@ -0,0 +1,10 @@
+package com.emilio;
+
+public class Counter {
+    public static void main(String[] args) {
+        int count;
+        for (count = 1; count < 11; count++) {
+            System.out.println(count);
+        }
+    }
+}
